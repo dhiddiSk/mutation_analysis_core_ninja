ce13a5ddc31024b280e87be8ba640ee42145e8c1
saikrishna <saikrishna.4369@gmail.com>
2019-05-14 23:24:53 +0200

    update

---
+++ src/test/java/com/practice/JunitsuiteClasses.java
@@ -0,0 +1,12 @@
+package com.practice;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        MessageUtilTest.class,
+        MessageUtilTest2.class
+})
+
+public class JunitsuiteClasses {
+}
